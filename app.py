091a9653e436d265c6b785aeb65e8ab180cd258e
GUNJANVERMA31 <24f2006762@ds.study.iitm.ac.in>
2026-08-03 14:33:17 +0530

    Connected login form with Flask

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        print("Email:", email)
+        print("Password:", password)
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
